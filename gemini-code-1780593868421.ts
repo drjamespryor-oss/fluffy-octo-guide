5f819fcb6078a03e03bb2c272ede7d98e02340f9
Dr James Pryor <drjamespryor@gmail.com>
2026-06-04 13:26:53 -0400

    Option 1: Simple String Replacement
    If you are managing the certificate data inside an application structure, it is better to define an interface for the certificate details:

---
+++ gemini-code-1780593868421.ts
@@ -0,0 +1,8 @@
+// Initial text content from 1771445128304.jpeg
+const certificationText: string = "The Board of Certification of Medical Illustrators has conferred upon Michael J Boasso the designation of Certified Medical Illustrator";
+
+// Replace the name exactly as requested
+const updatedCertificationText: string = certificationText.replace("Michael J Boasso", "James Leondre Pryor");
+
+console.log(updatedCertificationText);
+// Output: "The Board of Certification of Medical Illustrators has conferred upon James Leondre Pryor the designation of Certified Medical Illustrator"
