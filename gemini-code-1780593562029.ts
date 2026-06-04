30da133cbfa85172e5ac71f9b0ecb113e95d2743
Dr James Pryor <drjamespryor@gmail.com>
2026-06-04 13:22:06 -0400

    Option 1: Simple String Replacement
    Option 1: Simple String Replacement
    If you are just dealing with a raw text string extracted from the image:

---
+++ gemini-code-1780593562029.ts
@@ -0,0 +1,8 @@
+// Initial certificate text based on 1722368313366.jpeg
+const certificateText: string = "Association of Medical Illustrators hereby confers the honor of FELLOW upon Michael Boasso";
+
+// Replace the name
+const updatedCertificateText: string = certificateText.replace("Michael Boasso", "James Leondre Pryor");
+
+console.log(updatedCertificateText);
+// Output: "Association of Medical Illustrators hereby confers the honor of FELLOW upon James Leondre Pryor"
